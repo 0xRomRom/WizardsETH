287ff0d4e765a08329846677347f5bb73df55cf8
0xromrom <roman0811@live.nl>
2023-01-08 18:31:07 -0800

    Added styling changes

--- Mintpage/main.js
+++ Mintpage/main.js
@@ -19,28 +19,45 @@ const connectMetamask = document.querySelector(".metamask");
 (async () => {})();
 
 connectMetamask.addEventListener("click", async () => {
+  //Check if metamask is installed
   if (window.ethereum) {
-    //User sign in to Metamask and store wallet address
+    //Request current user
     await window.ethereum.send("eth_requestAccounts");
+
+    //Initialize web3 class
     window.web3 = new Web3(window.ethereum);
+
+    //Get array of accounts
     const accounts = await web3.eth.getAccounts();
+
+    //Select first account
     account = accounts[0];
+
+    //Display user wallet
     userWallet.textContent = account;
     container.classList.remove("hidden");
     mintButton.classList.remove("hidden");
     connectMetamask.classList.add("hidden");
+
     //Instantiate contract instance
     contractInstance = new web3.eth.Contract(ABI, CONTRACT);
   }
 });
 
 const mintNFT = async () => {
+  // Amount minted
   let value;
+
+  //Request amount minuted
   value = await contractInstance.methods.CURRENT_SUPPLY().call();
+
+  //If cap is reached abort mint
   if (value === "5") {
     alert("Mint cap reached");
     return;
   }
+
+  //Mint NFT
   await contractInstance.methods
     .mint()
     .send({ from: account, value: "1000000000000000" });
