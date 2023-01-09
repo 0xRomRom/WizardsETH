d6dca961415a73dc882bae60ccc896b72cc62844
0xromrom <roman0811@live.nl>
2023-01-08 17:48:57 -0800

    Mint functionality working

--- Mintpage/main.js
+++ Mintpage/main.js
@@ -2,7 +2,9 @@ import contractABI from "./abi.js";
 
 // Contract: 0x8EDD97Fc1665B7426a160463927694B37c4421b2
 
-window.onload = () => {};
+window.onload = () => {
+  connectMetamask.classList.remove("hidden");
+};
 
 let account;
 let contractInstance;
@@ -11,8 +13,12 @@ const CONTRACT = "0xe4d410eeAaB6597194223127D6c59b929f696aea";
 
 const userWallet = document.querySelector(".user-acc");
 const mintButton = document.querySelector(".mint-button");
+const container = document.querySelector(".cont");
+const connectMetamask = document.querySelector(".metamask");
 
-(async () => {
+(async () => {})();
+
+connectMetamask.addEventListener("click", async () => {
   if (window.ethereum) {
     //User sign in to Metamask and store wallet address
     await window.ethereum.send("eth_requestAccounts");
@@ -20,11 +26,13 @@ const mintButton = document.querySelector(".mint-button");
     const accounts = await web3.eth.getAccounts();
     account = accounts[0];
     userWallet.textContent = account;
-
+    container.classList.remove("hidden");
+    mintButton.classList.remove("hidden");
+    connectMetamask.classList.add("hidden");
     //Instantiate contract instance
     contractInstance = new web3.eth.Contract(ABI, CONTRACT);
   }
-})();
+});
 
 const mintNFT = async () => {
   let value;
